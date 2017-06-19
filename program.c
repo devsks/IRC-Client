b13cf6dc44fcfa5f860f927e5532adc769eb3b4f
devsks <coolnicesantosh@gmail.com>
2017-06-19 14:47:13 +0530

    commit

--- program.c
+++ program.c
@@ -1,7 +1,28 @@
-/*
-	this is a IRC client program which connects to irc.freenode.net
-*/
-/*	HEADERS	*/
+/*===---- devChat is a IRC client program ----------------------------===
+ *
+ * Copyright (c) 2017 Santosh Kumar Shaw
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ *===-----------------------------------------------------------------------===
+ */
+
 #include <stdio.h>
 #include <netdb.h>
 #include <limits.h>
@@ -11,13 +32,22 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
-// Handler for errors
-void quit(const char* msg)
+/*
+	quit() takes a string as argument and print it along
+	along with the error message. 
+*/
+void quit(const char *s)
 {
 	perror(msg);
 	exit(1);
 }
-
+/*
+	exitMessage() 
+*/
+void exitMessage(void)
+{
+	printf("devChat has beeb closed\n\n");
+}
 //struct  hostent {
   //           char    *h_name;        /* official name of host */
     //         char    **h_aliases;    /* alias list */
